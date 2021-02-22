dc3abefb0b7c63ef707bf77923df0f81c19c2422
ndirangu <ndirangu.muchai@gmail.com>
2021-02-22 21:11:15 +0300

    project setup

---
+++ src/main/java/com/ndirangu/superdrive/SuperDriveApplication.java
@@ -0,0 +1,13 @@
+package com.ndirangu.superdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperDriveApplication.class, args);
+    }
+
+}
